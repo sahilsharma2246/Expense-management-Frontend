7a513694367323c15a81d82bf815904e4f898a76
sahilsharma2246 <sahilwsharma2246@gmail.com>
2026-05-16 16:20:11 +0530

    updated new files

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -97,7 +97,7 @@ const HomePage = () => {
       getAllTransactions(); // ✅ refresh without reload
     } catch (error) {
       setLoading(false);
-      message.error('Failed to delete');
+      message.error('Failed to Delete');
     }
   };
 
